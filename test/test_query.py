8f80b7694e521812fecf01527cc3d41d1e7d9337
Yonatan Most <yomost@microsoft.com>
2019-07-22 20:50:50 +0300

    Add assert to test

--- test/test_query.py
+++ test/test_query.py
@@ -6,4 +6,7 @@ from query import Query, Order, Nulls
 
 class TestQuery(TestCase):
     def test_sanity(self):
-        print(Query().where(Col.foo > 4).take(5).sort_by(Col.bar, Order.ASC, Nulls.LAST).compile_all())
+        self.assertEqual(
+            Query().where(Col.foo > 4).take(5).sort_by(Col.bar, Order.ASC, Nulls.LAST).compile_all(),
+            " | where foo > 4 | take 5 | sort by bar asc nulls last"
+        )

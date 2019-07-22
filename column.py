20502762bf5c6b8a4e3624a3b1dc19a697c42caa
Yonatan Most <yomost@microsoft.com>
2019-07-22 13:29:17 +0300

    Support dot field names

--- column.py
+++ column.py
@@ -3,6 +3,10 @@ class Column:
     def __init__(self, name: str) -> None:
         self.name = name
 
+    # Using a string as the return type spec works around the circular reference problem
+    def __getattr__(self, name: str) -> 'Column':
+        return Column(self.name + '.' + name)
+
 
 class ColumnGenerator:
     def __getattribute__(self, name: str) -> Column:

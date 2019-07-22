41b5e8b1bf6f04e2b5f89072be5ec7cd737ffc86
Yonatan Most <yomost@microsoft.com>
2019-07-22 17:35:23 +0300

    Fix types

--- expressions.py
+++ expressions.py
@@ -4,10 +4,10 @@ from typing import Union
 from utils import KQL
 from utils import KustoTypes, to_kql
 
-StringExpressionTypes = Union[str, 'StringExpression', 'Column']
 ExpressionTypes = Union[KustoTypes, 'BaseExpression', 'Column']
-BooleanExpressionTypes = Union[bool, 'BooleanExpression', 'Column']
-NumberExpressionTypes = Union[int, float, 'NumberExpression', 'Column']
+StringTypes = Union[str, 'StringExpression', 'Column']
+BooleanTypes = Union[bool, 'BooleanExpression', 'Column']
+NumberTypes = Union[int, float, 'NumberExpression', 'Column']
 
 
 class BaseExpression:
@@ -26,64 +26,66 @@ class BaseExpression:
         return to_kql(obj)
 
     def __eq__(self, other: ExpressionTypes) -> 'BooleanExpression':
-        return BooleanExpression(self, ' == ', other)
+        return BooleanExpression.bi_operator(self, ' == ', other)
 
     def __ne__(self, other: ExpressionTypes) -> 'BooleanExpression':
-        return BooleanExpression(self, ' != ', other)
+        return BooleanExpression.bi_operator(self, ' != ', other)
 
     def is_in(self, other: ExpressionTypes) -> 'BooleanExpression':
-        return BooleanExpression(self, ' in ', other)
+        return BooleanExpression.bi_operator(self, ' in ', other)
 
     def __contains__(self, other: Any) -> bool:
         """
         Deliberately not implemented, because "not in" inverses the result of this method, and there is no way to
         override it
         """
-        raise NotImplementedError()
+        raise ValueError()  # Not raising NotImplementedError because then subclasses will be required to override it
 
 
 class BooleanExpression(BaseExpression):
-    def __init__(self, left: 'BooleanExpressionTypes', operator: str, right: 'BooleanExpressionTypes') -> None:
-        super().__init__(
-            KQL('{}{}{}'.format(self._subexpression_to_kql(left), operator, self._subexpression_to_kql(right)))
+    @staticmethod
+    def bi_operator(left: ExpressionTypes, operator: str, right: ExpressionTypes) -> 'BooleanExpression':
+        return BooleanExpression(
+            KQL('{}{}{}'.format(left._subexpression_to_kql(left), operator, right._subexpression_to_kql(right)))
         )
 
-    def __and__(self, other: BooleanExpressionTypes) -> 'BooleanExpression':
-        return BooleanExpressionTypes(self, ' and ', other)
+    def __and__(self, other: BooleanTypes) -> 'BooleanExpression':
+        return BooleanTypes(self, ' and ', other)
 
 
 class NumberExpression(BaseExpression):
-    def __init__(self, left: 'NumberExpression', operator: str, right: 'NumberExpression') -> None:
-        super().__init__(
-            KQL('{}{}{}'.format(self._subexpression_to_kql(left), operator, self._subexpression_to_kql(right)))
+    @staticmethod
+    def bi_operator(left: NumberTypes, operator: str, right: NumberTypes) -> 'NumberExpression':
+        return NumberExpression(
+            KQL('{}{}{}'.format(left._subexpression_to_kql(left), operator, right._subexpression_to_kql(right)))
         )
 
-    def __lt__(self, other: 'NumberExpression') -> BooleanExpression:
-        return BooleanExpression(self, ' < ', other)
+    def __lt__(self, other: NumberTypes) -> BooleanExpression:
+        return BooleanExpression.bi_operator(self, ' < ', other)
 
-    def __le__(self, other: 'NumberExpression') -> BooleanExpression:
-        return BooleanExpression(self, ' <= ', other)
+    def __le__(self, other: NumberTypes) -> BooleanExpression:
+        return BooleanExpression.bi_operator(self, ' <= ', other)
 
-    def __gt__(self, other: 'NumberExpression') -> BooleanExpression:
-        return BooleanExpression(self, ' > ', other)
+    def __gt__(self, other: NumberTypes) -> BooleanExpression:
+        return BooleanExpression.bi_operator(self, ' > ', other)
 
-    def __ge__(self, other: 'NumberExpression') -> BooleanExpression:
-        return BooleanExpression(self, ' >= ', other)
+    def __ge__(self, other: NumberTypes) -> BooleanExpression:
+        return BooleanExpression.bi_operator(self, ' >= ', other)
 
-    def __add__(self, other: 'NumberExpression') -> 'NumberExpression':
-        return NumberExpression(self, ' + ', other)
+    def __add__(self, other: NumberTypes) -> 'NumberExpression':
+        return NumberExpression.bi_operator(self, ' + ', other)
 
-    def __sub__(self, other: 'NumberExpression') -> 'NumberExpression':
-        return NumberExpression(self, ' - ', other)
+    def __sub__(self, other: NumberTypes) -> 'NumberExpression':
+        return NumberExpression.bi_operator(self, ' - ', other)
 
-    def __mul__(self, other: 'NumberExpression') -> 'NumberExpression':
-        return NumberExpression(self, ' * ', other)
+    def __mul__(self, other: NumberTypes) -> 'NumberExpression':
+        return NumberExpression.bi_operator(self, ' * ', other)
 
-    def __truediv__(self, other: 'NumberExpression') -> 'NumberExpression':
-        return NumberExpression(self, ' / ', other)
+    def __truediv__(self, other: NumberTypes) -> 'NumberExpression':
+        return NumberExpression.bi_operator(self, ' / ', other)
 
-    def __mod__(self, other: 'NumberExpression') -> 'NumberExpression':
-        return NumberExpression(self, ' % ', other)
+    def __mod__(self, other: NumberTypes) -> 'NumberExpression':
+        return NumberExpression.bi_operator(self, ' % ', other)
 
 
 class StringExpression(BaseExpression):

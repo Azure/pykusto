88c60e23b54fa5b70735bd52ade8a021a9bd78af
Yonatan Most <yomost@microsoft.com>
2019-07-22 21:00:49 +0300

    Fix requirements spec in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='pykusto',
     version='0.0.1',
-    packages=['azure-kusto-data==0.0.31'],
+    packages=[],
     url='https://dev.azure.com/yomost/_git/pykusto',
     license='MIT License',
     author='yomost',
     author_email='yomost@microsoft',
-    description='Advanced python SDK for Azure Data Explorer'
+    description='Advanced python SDK for Azure Data Explorer',
+    install_requires=['azure-kusto-data==0.0.31'],
 )

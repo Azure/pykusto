b9bc1d6bb2f37fe7e736fe0d7048970d1b4860a2
Yonatan Most <yomost@microsoft.com>
2019-07-23 08:42:27 +0300

    Simplify Table class

--- tables.py
+++ tables.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Union
 
 # noinspection PyProtectedMember
 from azure.kusto.data._response import KustoResponseDataSet
@@ -7,40 +7,20 @@ from azure.kusto.data.request import KustoClient
 from query import Query
 
 
-class BaseTable:
+class Table:
     client: KustoClient
     database: str
-
-    def __init__(self, client: KustoClient, database: str) -> None:
-        self.client = client
-        self.database = database
-
-    def execute(self, query: Query) -> KustoResponseDataSet:
-        raise NotImplementedError()
-
-
-class SingleTable(BaseTable):
     table: str
 
-    def __init__(self, client: KustoClient, database: str, table: str) -> None:
-        super().__init__(client, database)
-        if '*' in table:
-            raise ValueError("SingleTable does not support wildcards. Instead use UnionTable")
-        self.table = table
+    def __init__(self, client: KustoClient, database: str, tables: Union[str, Sequence[str]]) -> None:
+        self.client = client
+        self.database = database
+        if isinstance(tables, Sequence):
+            self.table = ', '.join(tables)
+        else:
+            self.table = tables
+        if '*' in self.table or ',' in self.table:
+            self.table = 'union ' + self.table
 
     def execute(self, query: Query) -> KustoResponseDataSet:
         return self.client.execute(self.database, self.table + query.compile_all())
-
-
-class UnionTable(BaseTable):
-    tables: Sequence[str]
-
-    def __init__(self, client: KustoClient, database: str, tables: Sequence[str]) -> None:
-        super().__init__(client, database)
-        self.tables = tables
-
-    def execute(self, query: Query) -> KustoResponseDataSet:
-        return self.client.execute(self.database, 'union {}{}'.format(
-            ', '.join(self.table),
-            query.compile_all()
-        ))

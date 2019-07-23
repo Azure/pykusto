fb604efcca8ba4fa826f6605c4671d32551a0c90
Amos Rimon <amrimon@microsoft.com>
2019-07-23 10:41:33 +0300

    adding all Kustos functions and marked the more important ones

--- test/test_query.py
+++ test/test_query.py
@@ -1,3 +1,4 @@
+from pykusto import functions as f
 from pykusto.column import columnGenerator as Col
 from pykusto.query import Query, Order, Nulls
 from test.test_base import TestBase
@@ -6,6 +7,6 @@ from test.test_base import TestBase
 class TestQuery(TestBase):
     def test_sanity(self):
         self.assertEqual(
-            Query().where(Col.foo > 4).take(5).sort_by(Col.bar, Order.ASC, Nulls.LAST).render(),
+            Query().where(f.abs(Col.foo) > 4).take(5).sort_by(Col.bar, Order.ASC, Nulls.LAST).render(),
             " | where foo > 4 | take 5 | sort by bar asc nulls last"
         )

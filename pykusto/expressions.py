f979a044c280e8bf5c477ff741188d11d8fc3400
Yonatan Most <yomost@microsoft.com>
2019-07-23 10:39:39 +0300

    Do not use native python casts

--- pykusto/expressions.py
+++ pykusto/expressions.py
@@ -63,7 +63,7 @@ class BaseExpression:
         """
         raise NotImplementedError("Instead use 'is_in' or 'contains'")
 
-    def __bool__(self) -> 'BooleanExpression':
+    def to_bool(self) -> 'BooleanExpression':
         return BooleanExpression(KQL('tobool({})'.format(self.kql)))
 
     def to_string(self) -> 'StringExpression':
@@ -172,7 +172,7 @@ class StringExpression(BaseExpression):
     def to_int(self) -> NumberExpression:
         return NumberExpression(KQL('toint({})'.format(self.kql)))
 
-    def __int__(self) -> NumberExpression:
+    def to_long(self) -> NumberExpression:
         return NumberExpression(KQL('tolong({})'.format(self.kql)))
 
     def lower(self) -> 'StringExpression':

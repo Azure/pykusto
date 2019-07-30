b5825abee7f27ed71cef39ff3e00f299df1a8ce6
Shai Keren <shkeren@microsoft.com>
2019-07-30 14:51:47 +0300

    move to github

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ _pykusto_ is an advanced Python SDK for Azure Data Explorer (a.k.a. Kusto).
 # Getting Started
 ### Installation
 ```bash
-pip install git+https://yomost@dev.azure.com/yomost/pykusto/_git/pykusto
+pip install git+https://github.com/Azure/pykusto.git
 ```
 
 ### Usage
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='pykusto',
     version='0.0.1',
     packages=find_packages(exclude=['test']),
-    url='https://dev.azure.com/yomost/_git/pykusto',
+    url='https://github.com/Azure/pykusto',
     license='MIT License',
     author='yomost',
-    author_email='yomost@microsoft',
+    author_email='yomost@microsoft.com',
     description='Advanced python SDK for Azure Data Explorer',
     install_requires=['azure-kusto-data==0.0.31'],
 )

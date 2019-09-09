003611a8b78ce1a625349d487bf284154e822969
Shai Keren <shkeren@microsoft.com>
2019-09-09 11:42:04 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pykusto',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(exclude=['test']),
     url='https://github.com/Azure/pykusto',
     license='MIT License',

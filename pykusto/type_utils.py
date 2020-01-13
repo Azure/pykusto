c51d2cc5220c071640ab88bdc0a0abf18d613389
Yonatan Most <>
2020-01-13 12:14:03 +0200

    #1 TypeRegistrar: improve, document and add tests

--- pykusto/type_utils.py
+++ pykusto/type_utils.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime, timedelta
 from numbers import Number
-from typing import Union, Mapping, Type, Dict, Callable, Any, Tuple, List
+from typing import Union, Mapping, Type, Dict, Callable, Tuple, List
 
 logger = logging.getLogger("pykusto")
 
@@ -10,32 +10,59 @@ KustoTypes = Union[str, Number, bool, datetime, Mapping, List, Tuple, timedelta]
 
 
 class TypeRegistrar:
+    """
+    A factory for annotations that are used to create a mapping between Kusto types and python types and functions.
+    Each annotation must be called with a Kusto type as a parameter. The `for_obj` and `for_type` methods
+    can then be used to retrieve the python type or function corresponding to a given Kusto type.
+    """
     registry: Dict[Type[KustoTypes], Callable]
 
-    def __init__(self) -> None:
+    def __init__(self, name: str) -> None:
+        """
+        :param name: Name is used for better logging and clearer errors
+        """
+        self.name = name
         self.registry = {}
+
+    def __repr__(self) -> str:
+        return self.name
 
     def __call__(self, *types: Type[KustoTypes]) -> Callable:
         def inner(wrapped):
             for t in types:
-                self.registry[t] = wrapped
+                previous = self.registry.setdefault(t, wrapped)
+                if previous is not wrapped:
+                    raise TypeError("{}: type already registered: {}".format(self, t.__name__))
             return wrapped
 
         return inner
 
-    def for_obj(self, obj: Any) -> Any:
+    def for_obj(self, obj: KustoTypes) -> Callable:
+        """
+        Given an object of Kusto type, retrieve the python type or function associated with the object's type, and call
+        it with the given object as a parameter
+
+        :param obj: An object of Kusto type
+        :return: Associated python object
+        """
         for registered_type, registered_callable in self.registry.items():
             if isinstance(obj, registered_type):
                 return registered_callable(obj)
-        raise ValueError("No registered callable for object {} of type {}".format(obj, type(obj).__name__))
+        raise ValueError("{}: no registered callable for object {} of type {}".format(self, obj, type(obj).__name__))
 
     def for_type(self, t: Type[KustoTypes]) -> Callable:
+        """
+        Given a Kusto type, retrieve the associated python type or function
+
+        :param t: A Kusto type
+        :return: Associated python object
+        """
         for registered_type, registered_callable in self.registry.items():
             if issubclass(t, registered_type):
                 return registered_callable
-        raise ValueError("No registered callable for type {}".format(t.__name__))
+        raise ValueError("{}: no registered callable for type {}".format(self, t.__name__))
 
 
-kql_converter = TypeRegistrar()
-plain_expression = TypeRegistrar()
-aggregation_expression = TypeRegistrar()
+kql_converter = TypeRegistrar("KQL Converter")
+plain_expression = TypeRegistrar("Plain expression")
+aggregation_expression = TypeRegistrar("Aggregation expression")
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,16 +1,81 @@
 from pykusto.expressions import to_kql
+from pykusto.type_utils import TypeRegistrar
 from test.test_base import TestBase
 
 
 class TestUtils(TestBase):
     def test_dynamic_to_kql(self):
-        dict ={
+        test_dict = {
             "name": "Alan",
             "age": 21,
             "address": ("NY", 36),
             "pets": ["Libby", "Panda", "]", "["]
         }
         self.assertEqual(
-            "{\"name\": \"Alan\", \"age\": 21, \"address\": (\"NY\", 36), \"pets\": (\"Libby\", \"Panda\", \"]\", \"[\")}",
-            to_kql(dict)
+            "{\"name\": \"Alan\", \"age\": 21, \"address\": (\"NY\", 36), "
+            "\"pets\": (\"Libby\", \"Panda\", \"]\", \"[\")}",
+            to_kql(test_dict)
+        )
+
+    def test_type_registrar_for_type(self):
+        test_annotation = TypeRegistrar("Test annotation")
+
+        @test_annotation(str)
+        def str_annotated(s: str) -> str:
+            return "response to " + s
+
+        self.assertEqual(
+            "response to test for_type",
+            test_annotation.for_type(str)("test for_type")
+        )
+
+    def test_type_registrar_for_obj(self):
+        test_annotation = TypeRegistrar("Test annotation")
+
+        @test_annotation(str)
+        def str_annotated(s: str) -> str:
+            return "response to " + s
+
+        self.assertEqual(
+            "response to test for_obj",
+            test_annotation.for_obj("test for_obj")
+        )
+
+    def test_type_registrar_for_type_not_found(self):
+        test_annotation = TypeRegistrar("Test annotation")
+
+        @test_annotation(str)
+        def str_annotated(s: str) -> str:
+            return "response to " + s
+
+        self.assertRaises(
+            ValueError("Test annotation: no registered callable for type bool"),
+            lambda: test_annotation.for_type(bool)("test for_type")
+        )
+
+    def test_type_registrar_for_obj_not_found(self):
+        test_annotation = TypeRegistrar("Test annotation")
+
+        @test_annotation(str)
+        def str_annotated(s: str) -> str:
+            return "response to " + s
+
+        self.assertRaises(
+            ValueError("Test annotation: no registered callable for object True of type bool"),
+            lambda: test_annotation.for_obj(True)
+        )
+
+    def test_type_registrar_collision(self):
+        test_annotation = TypeRegistrar("Test annotation")
+
+        @test_annotation(str)
+        def str_annotated_1(s: str) -> str:
+            return "response to " + s
+
+        def str_annotated_2(s: str) -> str:
+            return "response to " + s
+
+        self.assertRaises(
+            TypeError("Test annotation: type already registered: str"),
+            lambda: test_annotation(str)(str_annotated_2)
         )

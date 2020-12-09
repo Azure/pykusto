c0657587ca033082a16465944b729e8ffff59158
Yonatan Most <>
2020-12-09 15:52:42 +0200

    Modify fetching behavior to avoid confusing behavior if items are requested before they are done being fetched

--- pykusto/_src/item_fetcher.py
+++ pykusto/_src/item_fetcher.py
@@ -14,10 +14,10 @@ class _ItemFetcher(metaclass=ABCMeta):
     Abstract class that caches a collection of items, fetching them in certain scenarios.
     """
     _fetch_by_default: bool
+    __fetched: bool
     __items: Union[None, Dict[str, Any]]
     __future: Union[None, Future]
-    __item_write_lock: Lock
-    __item_fetch_lock: Lock
+    __items_lock: Lock
 
     def __init__(self, items: Union[None, Dict[str, Any]], fetch_by_default: bool) -> None:
         """
@@ -27,26 +27,23 @@ class _ItemFetcher(metaclass=ABCMeta):
         """
         self._fetch_by_default = fetch_by_default
         self.__items = items
+        self.__fetched = self.__items is not None
         self.__future = None
-        self.__item_write_lock = Lock()
-        self.__item_fetch_lock = Lock()
+        self.__items_lock = Lock()
 
     def _items_fetched(self) -> bool:
-        return self.__items is not None
+        return self.__fetched
 
     def _refresh_if_needed(self) -> None:
-        if self.__items is None and self._fetch_by_default:
+        if not self.__fetched and self._fetch_by_default:
             self.refresh()
 
     def _get_item_names(self) -> Generator[str, None, None]:
-        if self.__items is None:
-            return
-        # No race condition because once fetched self.__items will never go back to being None
+        self.blocking_refresh_if_needed()
         yield from self.__items.keys()
 
     def _get_items(self) -> Generator[Any, None, None]:
-        if self.__items is None:
-            return
+        self.blocking_refresh_if_needed()
         yield from self.__items.values()
 
     @abstractmethod
@@ -58,43 +55,62 @@ class _ItemFetcher(metaclass=ABCMeta):
         Convenience function for obtaining an item using dot notation.
         Often dot notation is used for other purposes, and sometimes that happens implicitly. For example Jupyter notebooks automatically run dot-notation code in the background
         on objects. For this reason, to avoid undesired erroneous queries sent to Kusto, an item is returned only if one already exists, and a new item is not generated otherwise
-        (in contrast to bracket notation).
+        (in contrast to bracket notation). If items have not been fetched yet, a time-restricted attempt will be made to fetch them.
 
         :param name: Name of item to return
         :return: The item with the given name
         :raises AttributeError: If there is no such item
         """
-        return self._get_item(name, lambda: _raise(AttributeError(f"{self} has no attribute '{name}'")))
+        return self._get_item(
+            name,
+            lambda: _raise(AttributeError(f"{self} has no attribute '{name}'")),
+            fetch_if_needed=True,
+            timeout_seconds=3,
+        )
 
     def __getitem__(self, name: str) -> Any:
         """
         Convenience function for obtaining an item using bracket notation.
-        Since bracket notation is only used explicitly, a new item is generated if needed (in contrast to dot notation).
+        Since bracket notation is only used explicitly, a new item is generated if needed (in contrast to dot notation). If items have not been fetched yet,
+        a time-restricted attempt will be made to fetch them.
 
         :param name: Name of item to return
         :return: The item with the given name
         """
-        return self._get_item(name, lambda: self.__generate_and_save_new_item(name))
+        return self._get_item(
+            name,
+            lambda: self.__generate_and_save_new_item(name),
+            fetch_if_needed=True,
+            timeout_seconds=3,
+        )
 
     def __generate_and_save_new_item(self, name: str) -> Any:
-        item = self._new_item(name)
-        with self.__item_write_lock:
+        with self.__items_lock:
             if self.__items is None:
                 self.__items = {}
-            self.__items[name] = item
-        return item
+            item = self.__items.get(name)
+            if item is None:
+                item = self._new_item(name)
+                self.__items[name] = item
+            return item
 
-    def _get_item(self, name: str, fallback: Callable[[], Any]) -> Any:
-        if self.__items is not None:
-            resolved_item = self.__items.get(name)
-            if resolved_item is not None:
-                return resolved_item
-        return fallback()
+    def _get_item(self, name: str, fallback: Callable[[], Any], fetch_if_needed: bool = False, timeout_seconds: Union[None, float] = None) -> Any:
+        if not self.__fetched:
+            if fetch_if_needed:
+                self.blocking_refresh(timeout_seconds)
+            else:
+                return fallback()
+
+        resolved_item = self.__items.get(name)
+        if resolved_item is None:
+            return fallback()
+        return resolved_item
 
     def __dir__(self) -> Iterable[str]:
         """
         Used by Jupyter for autocomplete
         """
+        self.blocking_refresh_if_needed(3)
         return sorted(chain(super().__dir__(), tuple() if self.__items is None else filter(lambda name: '.' not in name, self.__items.keys())))
 
     def refresh(self) -> None:
@@ -103,31 +119,32 @@ class _ItemFetcher(metaclass=ABCMeta):
         The specific logic for fetching is defined in concrete subclasses.
         """
         self.__future = _POOL.submit(self.__fetch_items)
-        self.__future.add_done_callback(self._set_items)
 
-    def wait_for_items(self) -> None:
+    def wait_for_items(self, timeout_seconds: Union[None, float] = None) -> None:
         """
         If item fetching is currently in progress, wait until it is done and return, otherwise return immediately.
         If several fetching threads are in progress, wait for the most recent one.
         """
         if self.__future is not None:
-            wait((self.__future,))
+            wait((self.__future,), timeout=timeout_seconds)
 
-    def blocking_refresh(self) -> None:
+    def blocking_refresh(self, timeout_seconds: Union[None, float] = None) -> None:
         self.refresh()
-        self.wait_for_items()
+        self.wait_for_items(timeout_seconds)
 
-    def _set_items(self, future: Future):
-        with self.__item_write_lock:
-            self.__items = future.result()
+    def blocking_refresh_if_needed(self, timeout_seconds: Union[None, float] = None) -> None:
+        if not self.__fetched and self._fetch_by_default:
+            self.refresh()
+            self.wait_for_items(timeout_seconds)
 
     @abstractmethod
     def _internal_get_items(self) -> Dict[str, Any]:
         raise NotImplementedError()  # pragma: no cover
 
-    def __fetch_items(self) -> Dict[str, Any]:
-        with self.__item_fetch_lock:
-            return self._internal_get_items()
+    def __fetch_items(self) -> None:
+        with self.__items_lock:
+            self.__items = self._internal_get_items()
+            assert self.__items is not None
 
 
 def _raise(e: BaseException):

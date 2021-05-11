8353c723c819223f5d1e5d0c34be2e1fd9306800
Yonatan Most <>
2021-05-11 12:41:33 +0300

    Revert stuff

--- setup.py
+++ setup.py
@@ -28,9 +28,7 @@ setup(
 
         # Release notes: https://pandas.pydata.org/docs/whatsnew/index.html
         # Note that starting from 1.2.0 support for Python 3.6 was dropped
-        'pandas>=0.25.0,<1.2.0',  # Tests use DataFrame constructor options introduced in 0.25.0
-
-        'numpy<1.20.0',  # After that support for Python 3.6 was dropped
+        'pandas>=0.25.0,<=1.2.4',  # Tests use DataFrame constructor options introduced in 0.25.0
 
         'redo==2.0.4',
     ],

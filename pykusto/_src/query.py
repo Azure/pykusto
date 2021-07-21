ba99c03ad92218e674a1f4df007de72f1b172e00
Yonatan Most <>
2021-07-21 11:15:02 +0300

    Fixing wrong types

--- pykusto/_src/query.py
+++ pykusto/_src/query.py
@@ -9,7 +9,7 @@ from .client import Table, KustoResponse, RetryConfig
 from .enums import Order, Nulls, JoinKind, Distribution, BagExpansion
 from .expressions import BooleanType, ExpressionType, AggregationExpression, OrderedType, \
     StringType, _AssignmentBase, _AssignmentFromAggregationToColumn, _AssignmentToSingleColumn, _AnyTypeColumn, \
-    BaseExpression, _AssignmentFromColumnToColumn, AnyExpression, _to_kql, _expression_to_type, BaseColumn, NumberType
+    BaseExpression, _AssignmentFromColumnToColumn, AnyExpression, _to_kql, _expression_to_type, BaseColumn, NumberType, _BooleanExpression
 from .functions import Functions as f
 from .kql_converters import KQL
 from .logger import _logger
@@ -49,7 +49,7 @@ class Query:
             new_object._head = self._head.__deepcopy__(memo)
         return new_object
 
-    def where(self, *predicates: BooleanType) -> 'Query':
+    def where(self, *predicates: Union[bool, _BooleanExpression]) -> 'Query':  # Can't use the 'BooleanType' shorthand because that fails typeguard for some reason
         """
         https://docs.microsoft.com/en-us/azure/data-explorer/kusto/query/whereoperator
 

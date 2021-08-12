a0ac76f99594b5f63b1db248313fb9b5ba746bd5
Yonatan Most <>
2021-08-12 15:11:31 +0300

    Create separate installation option for PySpark, this time using a different approach

--- pykusto-pyspark/setup.py
+++ pykusto-pyspark/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 from setup import core_requires, setup_kwargs
 
 setup_kwargs['name'] = 'pykusto-pyspark'
+setup_kwargs['packages'] = ['../pykusto']
 setup_kwargs['description'] = setup_kwargs['description'] + '(for PySpark)'
 setup_kwargs['install_requires'] = core_requires
 del setup_kwargs['extras_require']

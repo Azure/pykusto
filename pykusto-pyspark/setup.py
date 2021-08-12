6fc61b5861f7cb8986b0b2cdd2640a246adc87e6
Yonatan Most <>
2021-08-12 15:13:56 +0300

    Add comment

--- pykusto-pyspark/setup.py
+++ pykusto-pyspark/setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 from setup import core_requires, setup_kwargs
 
+# The 'pykusto-pyspark' package is identical to the pykusto 'package', except that it avoids dependencies which are not needed in PySpark
+# (we can't use 'extras_require' for this, because it does not allow negative dependencies)
+
 setup_kwargs['name'] = 'pykusto-pyspark'
 setup_kwargs['packages'] = ['../pykusto']
 setup_kwargs['description'] = setup_kwargs['description'] + '(for PySpark)'

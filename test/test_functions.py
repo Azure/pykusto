1b1db9a6db26335f25e722e54b90cbba665d9c09
IdanBrus <ibrusilovsky@microsoft.com>
2021-11-01 17:25:43 +0200

    add take_any function

--- test/test_functions.py
+++ test/test_functions.py
@@ -813,6 +813,24 @@ class TestFunction(TestBase):
             Query().summarize(f.make_set(t.stringField, t.numField)).render()
         )
 
+    def test_take_any_single_expr(self):
+        self.assertEqual(
+            " | summarize take_any(numField)",
+            Query().summarize(f.take_any(t.numField)).render()
+        )
+
+    def test_take_any_multiple_expr(self):
+        self.assertEqual(
+            " | summarize take_any(numField, stringField)",
+            Query().summarize(f.take_any(t.numField, t.stringField)).render()
+        )
+
+    def test_take_any_all(self):
+        self.assertEqual(
+            " | summarize take_any(*)",
+            Query().summarize(f.take_any_all()).render()
+        )
+
     def test_max(self):
         self.assertEqual(
             " | summarize max(numField)",
